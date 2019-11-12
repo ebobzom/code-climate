f0e4aa04e58553ac2fbd0de8baa13639726ae58d
Elijah Bobzom <ebobzom@gmail.com>
2019-11-13 00:20:37 +0100

    add email column

--- dbTable/table.js
+++ dbTable/table.js
@@ -10,6 +10,6 @@ const con = new pg.Pool({
 con.query('DROP table if exists users')
 .then(ans => console.log('ok'))
 .catch(e => console.log(e))
-con.query('CREATE TABLE users(id serial primary key not null, first_name varchar(50) not null, last_name varchar(50) not null)')
+con.query('CREATE TABLE users(id serial primary key not null, first_name varchar(50) not null, last_name varchar(50) not null, email varchar not null)')
 .then(ans => console.log('ok'))
 .catch(e => console.log(e))
